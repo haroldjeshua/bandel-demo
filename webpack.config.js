4d655f4fe6eb53fc4d5fd315be0e65aa7251c0fa
Harold Jeshua Varde <harvardii15@gmail.com>
2022-11-21 23:12:20 +0800

    loaders + sass compile

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,13 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].js",
   },
+
+  module: {
+    rules: [
+      {
+        test: /\/.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
